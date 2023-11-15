51e92e43c6ec43d58107292f828c8d3da9980bb6
P.Bor <p.bor@treecolor.spb.ru>
2023-11-15 21:12:20 +0300

    add new code with varibles type

---
+++ Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+int value = 100;
+Console.WriteLine(value);
+double d = 5.56;
+char C = 'A';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(C);
+Console.WriteLine(b);
