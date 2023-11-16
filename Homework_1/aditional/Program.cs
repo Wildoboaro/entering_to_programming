a10dc859fb311586844a3f74f1c904d4aec92a52
P.Bor <p.bor@treecolor.spb.ru>
2023-11-16 23:38:53 +0300

    add additional task HW_1

---
+++ Homework_1/aditional/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+long a = 456;
+long b = 782;
+long c = 918;
+long n = c;
+long result = n / 100 + (n - ((n / 100) * 100 + ((n - ((n / 100) * 100)) / 10) * 10));
+Console.WriteLine(result);
