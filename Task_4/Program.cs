e2d57b62615496db053a98efb1ca0462f5515974
P.Bor <p.bor@treecolor.spb.ru>
2023-11-16 17:12:00 +0300

    add task with finding the maximum value from 5

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -5,14 +5,30 @@ int b = 8;
 int c = 11;
 int d = 1;
 int e = 6;
-
-if (a > b)
+int max = a;
+// if (a > b)
+if (b > max)
 {
-    Console.Write("Большее из чисел: ");
-    Console.WriteLine(a);
-} 
-else
-{
-    Console.Write("Большее из чисел: ");
-    Console.WriteLine(b);
+//    Console.Write("Большее из чисел: ");
+//    Console.WriteLine(a);
+    max = b;
 }
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+// else
+//{
+//    Console.Write("Большее из чисел: ");
+//    Console.WriteLine(b);
+//}
+Console.Write("Большее из чисел: ");
+Console.WriteLine(max);
