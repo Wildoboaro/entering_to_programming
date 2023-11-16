a6c2bbce843a32f5608d986fc0738b70c65b9b59
P.Bor <p.bor@treecolor.spb.ru>
2023-11-16 23:07:38 +0300

    add 1st classwork

---
+++ classwork/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int a = 1;
+int b = 4;
+
+if (a == b * b)
+{
+    Console.WriteLine((a) + " является квадаратом " + (b));
+}
+else
+if (b == a * a)
+{
+    Console.WriteLine((b) + " является квадаратом " + (a));
+}
+else
+{
+    Console.WriteLine("Ни одно из чисел указанных не является квадаратом второго");
+}
+
