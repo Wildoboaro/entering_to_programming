976b565e65a8a2e51146bad9d6caf581eb8e4813
P.Bor <p.bor@treecolor.spb.ru>
2023-11-16 17:06:10 +0300

    add task with finding the maximum value

---
+++ Task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int a = 3;
+int b = 8;
+int c = 11;
+int d = 1;
+int e = 6;
+
+if (a > b)
+{
+    Console.Write("Большее из чисел: ");
+    Console.WriteLine(a);
+} 
+else
+{
+    Console.Write("Большее из чисел: ");
+    Console.WriteLine(b);
+}
