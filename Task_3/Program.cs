0684d06326d3aeedea1fdba21192cd31d882c5e6
P.Bor <p.bor@treecolor.spb.ru>
2023-11-16 16:39:05 +0300

    add task with arifmetic functions

---
+++ Task_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int i1 = 21;
+int i2 = 3;
+long l1 = 7;
+double d1 = 8.9;
+  int sum = i1 + i2;
+  long sum_l = i1 + l1;
+  double sum_d = i1 + d1;  
+
+  int product = i1 * i2;
+  long product_l = i1 * l1;
+  double product_d = i1 * d1;  
+
+  int quotient = i1 / i2;
+  long quotient_l = i1 / l1;
+  double quotient_d = i1 * d1;  
+
+Console.Write("Сумма простых чисел: ");
+Console.WriteLine(sum);
+Console.Write("Сумма простого м целочисденного: ");
+Console.WriteLine(sum_l);
+Console.Write("Сумма простого и числа с дробной частью: ");
+Console.WriteLine(sum_d);
+Console.Write("Произведение простых чисел: ");
+Console.WriteLine(product);
+Console.Write("Произведение простого м целочисденного: ");
+Console.WriteLine(product_l);
+Console.Write("Произведение простого и числа с дробной частью: ");
+Console.WriteLine(product_d);
+Console.Write("Результат деления простых чисел: ");
+Console.WriteLine(quotient);
+Console.Write("Результат деления простого на целочисденное: ");
+Console.WriteLine(quotient_l);
+Console.Write("Результат деления простого на числа с дробной частью: ");
+Console.WriteLine(quotient_d);
