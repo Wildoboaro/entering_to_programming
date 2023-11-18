93832fc975f092d9139684dadb7c155befeb2281
P.Bor <p.bor@treecolor.spb.ru>
2023-11-18 16:28:54 +0300

    add third parts of first homework

---
+++ Homework_1_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// мои дополнения
+int number;
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+// Контрольный вывод переменных на экран
+Console.WriteLine($"number:  '{number}'");
+// решение мое
+// if ((number/2)*2 == number)
+//{
+//    Console.WriteLine($"Число `{number}` чётное");
+//}
+//else
+//{
+//    Console.WriteLine($"Число `{number}` нечётное");
+//}
+// решение "правильное"
+if (number % 2 == 0)
+{
+    Console.WriteLine($"Число `{number}` чётное");
+}
+else
+{
+    Console.WriteLine($"Число `{number}` нечётное");
+}
