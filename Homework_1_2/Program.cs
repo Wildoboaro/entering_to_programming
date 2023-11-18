43c144d9a257bbb17eb5f28495399677c3100ddd
P.Bor <p.bor@treecolor.spb.ru>
2023-11-18 16:06:44 +0300

    add second parts of first homework

---
+++ Homework_1_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// мои дополнения
+int a;
+int b;
+int c;
+Console.Write("Введите первое число:  ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:  ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе третье: ");
+c = Convert.ToInt32(Console.ReadLine());
+// домашка 1 часть 1
+// Контрольный вывод переменных на экран
+//Console.WriteLine($"firstNumber:  '{a}'");
+//Console.WriteLine($"secondNumber: '{b}'");
+//Console.WriteLine($"thirdNumber:  '{c}'");
+// Решение
+int result = a;
+if (b > result)
+{
+    result = b;
+}
+if (c > result)
+{
+    result = c;
+}
+Console.WriteLine($"Максимальоне число:   '{result}'");
