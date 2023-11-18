ee6fc73b7a0bd7e107ce718979e57f8661ad52dc
P.Bor <p.bor@treecolor.spb.ru>
2023-11-18 16:45:49 +0300

    add fourth parts of first homework

---
+++ Homework_1_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// мои дополнения
+int number;
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+// Контрольный вывод переменных на экран
+// Console.WriteLine($"number:  '{number}'");
+// решение
+int evenNumber = 2;
+while (evenNumber <= number)
+{
+    Console.Write($"{evenNumber}	");
+    evenNumber = evenNumber + 2;
+}
