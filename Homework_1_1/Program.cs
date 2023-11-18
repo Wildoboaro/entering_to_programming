2c3106676e8b5979e847e4651165d97b2499dc6e
P.Bor <p.bor@treecolor.spb.ru>
2023-11-18 15:42:14 +0300

    add first parts of first homework

---
+++ Homework_1_1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// мои дополнения
+int firstNumber;
+int secondNumber;
+Console.Write("Введите первое число: ");
+firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:: ");
+secondNumber = Convert.ToInt32(Console.ReadLine());
+// домашка 1 часть 1
+// Контрольный вывод переменных на экран
+//Console.WriteLine($"firstNumber: '{firstNumber}'");
+//Console.WriteLine($"secondNumber: '{secondNumber}'");
+// Решение
+if (firstNumber < secondNumber)
+{
+    Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
+}
+else
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine($"Второе число `{secondNumber}` меньше чем первое число `{firstNumber}`");
+}
+else
+{
+    Console.WriteLine($"Введенные числа равны `{firstNumber}`");
+}
