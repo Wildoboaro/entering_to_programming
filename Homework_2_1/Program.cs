0bb2e867f78369ff81c48e2002ade365bbb2e8b4
Pavel_Bor <wildoboaro@gmail.com>
2023-11-21 15:41:41 +0300

    adding homework 2 part 1

---
+++ Homework_2_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System; 
+using System.Linq;
+ 
+namespace Homework_2_1; 
+ 
+public static class Program 
+{
+ public static void Main()
+ {
+    int num;
+    Console.Write("Введите введите число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    if ((num % 7 == 0) & (num % 23 == 0))
+    {
+     Console.WriteLine("Да, число делется и на 7 и на 23");
+    } 
+    else
+    {
+     Console.WriteLine("Нет, это число не делится одновременно и на 7 и на 23");
+    }
+ }
+}
---
+++ Homework_2_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System; 
+using System.Linq;
+ 
+namespace Homework_2_2; 
+ 
+public static class Program 
+{
+ public static void Main()
+ {
+  int x;
+  int y;
+  Console.Write("Введите координату X:");
+  x = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите координату Y:");
+  y = Convert.ToInt32(Console.ReadLine());
+  if ( x == 0 & y == 0 )
+  {
+   Console.WriteLine("Точка лежит на переснчении оси координат");   
+  }
+  else
+  {
+  if ( x == 0 )
+  { 
+   Console.WriteLine("Точка лежит на оси Y");
+  }
+  if ( y == 0 )
+  {
+   Console.WriteLine("Точка лежит на оси X"); 
+  }
+  if ( x > 0 & y > 0)
+  {
+  Console.WriteLine("Точка лежит в первой четверти стстемы коодинат");  
+  }
+  if ( x > 0 & y < 0)
+  {
+  Console.WriteLine("Точка лежит во вторй четверти систмы координат"); 
+  }
+  if ( x < 0 & y < 0)
+  {
+  Console.WriteLine("Точка лежит в третьей четверти системы коордтнат"); 
+  }
+  if ( x < 0 & y > 0)
+  {
+  Console.WriteLine("Точка лежит в четвертой четверти системы кооржинат");  
+  }
+  }  
+ } 
+}
