1f456e4576d939338ad4a04a153922cc6ede4a03
Pavel_Bor <wildoboaro@gmail.com>
2023-11-21 16:20:58 +0300

    adding to homework 2 part 1 some feature

--- Homework_2_1/Program.cs
+++ Homework_2_1/Program.cs
@@ -12,11 +12,28 @@ public static class Program
     num = Convert.ToInt32(Console.ReadLine());
     if ((num % 7 == 0) & (num % 23 == 0))
     {
-     Console.WriteLine("Да, число делется и на 7 и на 23");
+         Console.WriteLine("Да, число делется и на 7 и на 23");
     } 
     else
     {
      Console.WriteLine("Нет, это число не делится одновременно и на 7 и на 23");
+// for additional checking
+//        if ((num % 7 == 0))
+//        {
+//            Console.WriteLine("Число делется на 7, но не делится на 23");
+//        }
+//        else
+//        {
+//            if ((num % 23 == 0))
+//            {
+//                Console.WriteLine("Число не делется на 7, но делится на 23");
+//            
+//           }
+//            else
+//            {
+//                Console.WriteLine("Число не делется на 7 м не делится на 23");       
+//        }
+//        }
     }
  }
 }
--- Homework_2_1/obj/Debug/net8.0/Homework_2_1.AssemblyInfo.cs
+++ Homework_2_1/obj/Debug/net8.0/Homework_2_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework_2_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b91cdaccc17509475023a68945a89ddc99e70a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9f3633ac7c114cafb76e0c2d697d85f75252c71")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework_2_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework_2_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
