41c6ddae2111c6f589a3d8982325eca7ac491e04
Pavel_Bor <wildoboaro@gmail.com>
2023-11-22 17:12:53 +0300

    trying to solve task 1

---
+++ codewars_1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Kata.ArrayDiff(new int[] {1, 2}, new int[] {1}) => new int[] {2}
+int[] array1 = [1, 2, 3, 4, 5, 6];
+int[] array2 = [2, 5];
+int [] array3 = new int [4];
+int count1 = 0;
+int count3 = 0;
+
+while (count1 < array1.Length) 
+{
+    bool dif = true;
+    int count2 = 0;
+    while (count2 < array2.Length)
+    {
+        if (array1[count1] == array2[count2])
+        {
+            dif = false;
+        }
+        count2 = count2 + 1;
+    }
+    if (dif)
+    {
+        array3[count3] = array1[count1];
+        count3 = count3 + 1;
+    }
+           
+    count1 = count1 + 1;
+}
+
+
+// print result
+count3 = 0;
+while (count3 < array3.Length)
+{
+    Console.Write($"{array3[count3]} ");
+    count3 = count3 + 1; 
+}
