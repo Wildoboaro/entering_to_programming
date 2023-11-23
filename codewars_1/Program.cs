5c216fbccdd6407712507be19d053b3ef6afcedc
Pavel_Bor <wildoboaro@gmail.com>
2023-11-23 20:34:15 +0300

    second stage of first task codewar

--- codewars_1/Program.cs
+++ codewars_1/Program.cs
@@ -1,9 +1,42 @@
 ﻿// Kata.ArrayDiff(new int[] {1, 2}, new int[] {1}) => new int[] {2}
+
 int[] array1 = [1, 2, 3, 4, 5, 6];
 int[] array2 = [2, 5];
-int [] array3 = new int [4];
+// int [] array3 = new int [4];
 int count1 = 0;
 int count3 = 0;
+int same_size = array1.Length;
+
+// founding diff  arroy size
+
+while (count1 < array1.Length) 
+{
+    bool dif = true;
+    int count2 = 0;
+    while (count2 < array2.Length)
+    {
+        if (array1[count1] == array2[count2])
+        {
+            dif = false;
+        }
+        count2 = count2 + 1;
+    }
+    if (dif)
+    {
+        same_size = same_size - 1;
+        count3 = count3 + 1;
+    }
+           
+    count1 = count1 + 1;
+}
+
+// it just  for testing
+Console.WriteLine($"Количество одинаковых элементов: {same_size}");
+
+int [] array3 = new int [array1.Length - same_size];
+count1 = 0;
+count3 = 0;
+// creating diff array
 
 while (count1 < array1.Length) 
 {
@@ -27,7 +60,8 @@ while (count1 < array1.Length)
 }
 
 
-// print result
+// printing result
+
 count3 = 0;
 while (count3 < array3.Length)
 {
