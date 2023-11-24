f23acbb269ce3dc3aebc46989a7ce70a3e070af8
Pavel_Bor <wildoboaro@gmail.com>
2023-11-24 19:32:33 +0300

    one step in codewars_3

--- codewars_3/Program.cs
+++ codewars_3/Program.cs
@@ -1,2 +1,14 @@
 ﻿// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
 // 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int sum = num1 + num2;
+string res;
+while sum % 2 !=1
+{
+    res = res   
+}
