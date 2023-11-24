ae44d6bbf99042be44ea2532dd1f7091b4035fe7
Pavel_Bor <wildoboaro@gmail.com>
2023-11-24 15:55:23 +0300

    add some more solutied task

---
+++ classwork_3_3/Program.cs
@@ -0,0 +1,16 @@
+﻿int FactorialFunc(int a)
+{
+    int product = 1;
+    while (a > 1)
+    {
+        product = product * a;
+        a = a - 1; 
+    }
+    return product;
+}
+
+
+Console.Write("Введите число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+int Res = FactorialFunc(Num);
+Console.WriteLine($"Произведение чисел от 1 до {Num} равна {Res}");
---
+++ classwork_3_4/Program.cs
@@ -0,0 +1,25 @@
+﻿void SqrsFunc(int a)
+{
+    int sqrs = 1;
+    if (a == 0)
+    {    
+        Console.Write("вы ввели ноль");
+    }
+    else
+    {
+        Console.Write(sqrs);
+        if (a > 1)
+        {
+            while (sqrs < a)
+            {
+                sqrs = sqrs + 1;
+                Console.Write($", {sqrs*sqrs}");
+            }
+        }
+    }
+}
+
+
+Console.Write("Введите число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+SqrsFunc(Num);
