b6a5d974a2d5507a478a4d90cbaf1dec547d32d0
Pavel_Bor <wildoboaro@gmail.com>
2023-11-24 13:59:28 +0300

    add some solutied task

--- codewars_1/Program.cs
+++ codewars_1/Program.cs
@@ -1,72 +1,71 @@
 ﻿// Kata.ArrayDiff(new int[] {1, 2}, new int[] {1}) => new int[] {2}
+// data for test
+int[] array1 = [1, 2, 2, 2, 5, 6, 1, 25, 16, 256];
+int[] array2 = [2, 5, 25];
 
-int[] array1 = [1, 2, 2, 2, 5, 6];
-int[] array2 = [2, 5];
-// int [] array3 = new int [4];
-
-int count1 = 0;
-int count3 = 0;
-function int[] Kata.ArrayDiff(int[], int[]);
-{
-// founding diff  arroy size
-int same_size = array1.Length;
-while (count1 < array1.Length) 
-{
-    bool dif = true;
-    int count2 = 0;
-    while (count2 < array2.Length)
+    static int[] ArrayDiff(int[] a, int[] b)
     {
-        if (array1[count1] == array2[count2])
+        // founding diff arroy size
+        int count1 = 0;
+        int count3 = 0;
+        int same_size = a.Length;
+        while (count1 < a.Length) 
         {
-            dif = false;
-        }
-        count2 = count2 + 1;
-    }
-    if (dif)
-    {
-        same_size = same_size - 1;
-        count3 = count3 + 1;
-    }
+            bool dif = true;
+            int count2 = 0;
+            while (count2 < b.Length)
+            {
+                if (a[count1] == b[count2])
+                {
+                    dif = false;
+                }
+                count2 = count2 + 1;
+            }
+            if (dif)
+            {
+                same_size = same_size - 1;
+                count3 = count3 + 1;
+            }
            
-    count1 = count1 + 1;
-}
+            count1 = count1 + 1;
+        }
 
-// it just  for testing
-Console.WriteLine($"Количество одинаковых элементов: {same_size}");
+        // it just  for testing
+        //Console.WriteLine($"Количество одинаковых элементов: {same_size}");
 
-int [] array3 = new int [array1.Length - same_size];
-count1 = 0;
-count3 = 0;
-// creating diff array
+        int [] a3 = new int [a.Length - same_size];
+        count1 = 0;
+        count3 = 0;
+        // creating diff array
 
-while (count1 < array1.Length) 
-{
-    bool dif = true;
-    int count2 = 0;
-    while (count2 < array2.Length)
-    {
-        if (array1[count1] == array2[count2])
+        while (count1 < a.Length) 
         {
-            dif = false;
-        }
-        count2 = count2 + 1;
-    }
-    if (dif)
-    {
-        array3[count3] = array1[count1];
-        count3 = count3 + 1;
-    }
+            bool dif = true;
+            int count2 = 0;
+            while (count2 < b.Length)
+            {
+                if (a[count1] == b[count2])
+                {
+                    dif = false;
+                }
+                count2 = count2 + 1;
+            }
+            if (dif)
+            {
+                a3[count3] = a[count1];
+                count3 = count3 + 1;
+            }
            
-    count1 = count1 + 1;
-}
-return array3;
-}
+            count1 = count1 + 1;
+        }
+        return a3;
+    }
 
 // printing result
-int[] array_diff = Kata.ArrayDiff(array1, array2); 
-count3 = 0;
-while (count3 < array_diff.Length)
+int[] array_diff = ArrayDiff(array1, array2); 
+int  count_print = 0;
+while (count_print < array_diff.Length)
 {
-    Console.Write($"{array_diff[count3]} ");
-    count3 = count3 + 1; 
+    Console.Write($"{array_diff[count_print]} ");
+    count_print = count_print + 1; 
 }
