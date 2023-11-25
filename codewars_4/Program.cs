bbafceed69e70924213bb134b181eea05dbadb1d
P.Bor <p.bor@treecolor.spb.ru>
2023-11-25 17:47:37 +0300

    commit

---
+++ codewars_4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите число 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 0)
+{
+    Console.WriteLine($"Число {n} меньше единцы");
+//    return false;
+} 
+else
+{
+    int s = 4;
+    int k = 1;
+    int m = Convert.ToInt32(Math.Pow(2, n)) - 1;
+    while (k != n - 1)
+    {
+        s = ((s * s) - 2) * m;
+        k += 1;
+    }
+    if (s == 0)
+    {
+        Console.WriteLine($"Число {n} простое");
+//    return true;
+    } 
+    else
+    { 
+        Console.WriteLine($"Число {n} соствное");
+//    return false;
+    }
+
+}
