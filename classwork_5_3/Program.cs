35f555d2ef2d38632376a274c7d7729fa7e585d3
P.Bor <wildoboaro@gmail.com>
2023-12-04 23:41:31 +0300

    add classworks 5

---
+++ classwork_5_3/Program.cs
@@ -0,0 +1,21 @@
+﻿int GetCountVovelsInString(string str1, string str2)
+{
+int count = 0;
+foreach(char str1_elem in str1)
+{
+    foreach(char str2_elem in str2)
+    {
+        if(str1_elem == str2_elem)
+        {
+            count++;
+        }
+    }
+}
+return count;
+}
+
+
+string str1 = "world";
+string str2 = "wrtgfhjdid";
+int count = GetCountVovelsInString(str1, str2);
+Console.WriteLine(count);
---
+++ classwork_5_4/Program.cs
@@ -0,0 +1,34 @@
+﻿string FilterString(string input_string)
+{
+    string lower_string = input_string.ToLower();
+    string output_string = "";
+    foreach(char elem_str in lower_string)
+    {
+        if(elem_str != ' ')
+        {
+            output_string+=elem_str;
+        }
+    }
+    return output_string;
+}
+
+
+bool IsPalindrome(string input_string)
+{
+    int len = input_string.Length;
+    for (int i = 0; i < input_string.Length/2; i++)
+    {
+        if (input_string[i] != input_string[input_string.Length-i-1])
+            return false;
+    }
+    return true;
+}
+
+
+
+string str = "Goig";
+str = FilterString(str);
+if (IsPalindrome(str)) 
+Console.WriteLine("Полендром");
+else
+Console.WriteLine("Не полендром");
